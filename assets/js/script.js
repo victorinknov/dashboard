cb1fa1514545b7e4e5f128a589f670c957950ab8
Victor Inknov <victorhonly@gmail.com>
2022-03-13 19:12:54 -0300

    adding modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log('passou aqui')
+let gear = document.getElementById('gear')
+let userImg = document.getElementById('user-picture')
+let modalConfig = document.getElementById('modal-config')
+let closeModal = document.getElementById('close-button')
+
+userImg.addEventListener('mouseover', ()=>{
+    gear.style.visibility = 'visible'
+})
+
+gear.addEventListener('mouseover', ()=>{
+    gear.style.visibility = 'visible'
+})
+
+userImg.addEventListener('mouseleave', ()=>{
+    gear.style.visibility = 'hidden'
+})
+
+gear.addEventListener('click', ()=>{
+    modalConfig.style.visibility = 'visible'
+})
+
+closeModal.addEventListener('click', ()=>{
+    modalConfig.style.visibility = 'hidden'
+})
