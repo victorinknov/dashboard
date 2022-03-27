aacf249cbe3b9766cc56fa7e3444b9a897d83f8a
Victor Inknov <victorhonly@gmail.com>
2022-03-27 09:55:34 -0300

    cooler animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,36 @@ searchBtn.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
 })
 
+let clickCounter = 0
+logo.addEventListener('click', ()=>{
+    clickCounter ++
+    console.log(clickCounter)
+    switch (clickCounter) {
+        case 10:
+            logo.classList.add('infinite-spin', 'normal')
+            break;
+        case 25:
+                logo.classList.add('infinite-spin', 'fast')
+                break;
+        case 50:
+                logo.classList.add('infinite-spin', 'faster')
+                break;
+        case 50:
+                logo.classList.add('infinite-spin', 'faster')
+                break;
+        case 100:
+                logo.classList.add('infinite-spin', 'fucking-faster')
+                break;
+        case 125:
+                logo.classList.remove('infinite-spin', 'normal', 'fast', 'faster', 'fucking-faster')
+                window.alert('Parabéns, bastardo. Você quebrou o cooler.')
+                break;
+        default:
+            break;
+    }
+    
+})
+
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
     
@@ -29,3 +59,6 @@ modeSwitch.addEventListener("click" , () =>{
 });
 
 // easter egg 
+
+
+console.log(modeSwitch.value)
